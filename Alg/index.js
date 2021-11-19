059c488311242ca70a7eb8460a14cd35d72403e6
Dmitriy Zhyhylii <dmitry.zhigiliy@gmail.com>
2021-11-20 01:34:41 +0200

    wrote: sum

--- Alg/index.js
+++ Alg/index.js
@@ -1,1 +1,27 @@
 "use strict"
+
+function sum(obj, sumOfValues = 0) {
+    if (!Object.keys(obj)) {
+        return sumOfValues
+    }
+
+    for (const key in obj) {
+        if (typeof obj[key] === 'object') {
+            return sum(obj[key], sumOfValues)
+        } else {
+            sumOfValues += obj[key]
+        }
+    }
+
+    return sumOfValues
+}
+
+console.log(sum({
+    a: {
+        b: 1,
+        c: {
+            e: 2,
+            d: 10
+        }
+    }
+}))
